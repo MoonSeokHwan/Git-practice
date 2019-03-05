988d6327f18c0d5e373dfef885e57ae1cf8ca3af
MOONSH <msh8014@gmail.com>
2019-03-06 00:09:30 +0900

    2019.03.06

--- Algorithms/add.swift
+++ Algorithms/add.swift
@@ -2,40 +2,28 @@
 (첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
 */
 
+//Answer #1 
 import Foundation
 
-print("Please input the values")
+var inputNum = readLine()! // string? type
+var array = inputNum.components(separatedBy:" ").map{Int($0)!}
 
-var num1 = readLine()!
-var num2 = readLine()!
-var int1 = Int(num1)
-var int2 = Int(num2)
+print(array[0]+array[1])
 
-if let a = int1{
-if let b = int2{
-    if a > 0 && b < 10{
-        let result = a+b
-        print(result)
-    } else {
-        print("Please retry it!")      
-    }   
-}
-}
-/*
-import Foundation
 
-print("Please input the values")
 
-var a = readLine()
-if let num = a {
-    var array = num.components(separatedBy:" ")
-    if Int(array[0])! > 0 && Int(array[1])! < 10 {
-        let add = Int(array[0])! + Int(array[1])!
-    print(add)
-    }
-    else {
-        print("Please retry")
-    }
+//Answer #2
+// import Foundation
+
+// var a = readLine()
+// if let num = a {
+//     var array = num.components(separatedBy:" ")
+//     if Int(array[0])! > 0 && Int(array[1])! < 10 {
+//         let add = Int(array[0])! + Int(array[1])!
+//     print(add)
+//     }
+//     else {
+//         print("Please retry")
+//     }
     
-}
-*/
+// }
